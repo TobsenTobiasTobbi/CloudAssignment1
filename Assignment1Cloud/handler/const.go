0562341f4c67d2b2e191147197a63d5184998839
TobiasHLarsen <tobiashl@stud.ntnu.no>
2026-02-06 13:56:01 +0100

    some actual code

--- Assignment1Cloud/handler/const.go
+++ Assignment1Cloud/handler/const.go
@@ -1,6 +1,6 @@
-package handlers
+package handler
 
 // URL paths as constants, professor suggested this
-const status_path = "/countryinfo/v1/status/"
-const info_path = "/countryinfo/v1/info/"
-const exchange_path = "/countryinfo/v1/exchange/"
+const STATUS_PATH = "/countryinfo/v1/status/"
+const INFO_PATH = "/countryinfo/v1/info/"
+const EXCHANGE_PATH = "/countryinfo/v1/exchange/"
